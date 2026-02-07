15ccf6c9e7aaba276d84a94e873e85dfd976ae47
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2026-02-07 21:54:42 +0700

    fix:delete console store

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -10,9 +10,5 @@ export const store = configureStore({
   },
 });
 
-store.subscribe(() => {
-  console.info(store.getState().sourceSlice);
-});
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
